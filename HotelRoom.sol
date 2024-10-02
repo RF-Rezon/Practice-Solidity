2dba66f252555f50f6431810de2873496190e7cd
Rejwan Faysal Rezon <97096118+RF-Rezon@users.noreply.github.com>
2024-10-02 14:57:21 +0600

    HotelRoom

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -1,1 +1,41 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract HotelRoom {
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses public currentStatus;
+
+    event Occupy(address _occupant, uint _value);
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant() {
+        //Check Status
+        require(currentStatus == Statuses.Vacant, "Status is Occupied.");
+        _;
+    }
+
+    modifier checkPrice(uint _amount) {
+        //Check Price
+        require(msg.value >= _amount, "Not enough ether.");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant checkPrice(2 ether){
+        currentStatus = Statuses.Occupied;
+
+        (bool sent, bytes memory data) = owner.call{value: msg.value}("");
+        require(true);
+
+        emit Occupy(msg.sender, msg.value);
+    }
+}
