3b11b3d1bb595b6b18f5bd36c6c2d6a44b05ef02
Rejwan Faysal Rezon <97096118+RF-Rezon@users.noreply.github.com>
2024-10-11 01:40:17 +0600

    v2

--- SmartWallet.sol
+++ SmartWallet.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.25;
 contract SmartContractWallet {
     address payable owner;
 
+    mapping (address => uint) public allowance;
+    mapping (address => bool) public isAllowedToSend;
+
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    function setAllowance(address _for, uint _ammount) public {
+         require(msg.sender == owner, "You are not the owner");
+         allowance[msg.sender] = _ammount;
+         if(_ammount > 0){
+            isAllowedToSend[_for] = true;
+         }else{
+            isAllowedToSend[_for] = false;
+         }
     }
 
     function transfer(
@@ -13,7 +26,12 @@ contract SmartContractWallet {
         uint256 _ammount,
         bytes memory _payload
     ) public returns (bytes memory) {
-        require(msg.sender == owner, "You are not the owner");
+       if(msg.sender != owner){
+        require(isAllowedToSend[msg.sender], "You are not allowed to send anything from this smart contract.");
+        require(allowance[msg.sender] >= _ammount, "You are not allowed to send anything from this smart contract.");
+
+        allowance[msg.sender] -= _ammount;
+       }
         (bool success, bytes memory returnData) = _to.call{value: _ammount}( 
             _payload
         );
